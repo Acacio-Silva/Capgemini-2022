02d717f98392b6b96942d2efab4a3ecebd075149
Acacio-silva <acaciosilva90@gmail.com>
2022-03-04 16:04:54 -0300

    inclusão do readme.md e comentarios na classe main

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Desafio de programação
+
+### Tecnologias utilizadas:
+ - Java 
+ - Maven
+ - JUnit 5
+
+#### Para ver o resultado das questões basta importar o projeto no eclipse, ou outra IDE que tenha suporte a java e executar a classe Main.java;
+
+#### Para executar os testes basta executar um dos arquivos de teste no pacote "src/test/java/main";
+
+#### Os resultados da execusão dos codigos seram exibidos no console da IDE;
+
+#### Cada questão está separada em uma classe no pacote main, para vizualizar as implementações basta escolher uma ficar a vontade.
+
+
+
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,22 +7,22 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		List<Integer> list =  Arrays.asList(9, 2, 1, 4, 6);
-		System.out.println("PRIMEIRA QUESTAO");
+		System.out.println("PRIMEIRA QUESTAO"); // Segunda  questão
+		List<Integer> list =  Arrays.asList(9, 2, 1, 4, 6); // Lista com os valores 
 		Questao01 questao01 = new Questao01();
-		questao01.mediana(list);
+		questao01.mediana(list); // Metodo mediana da classe questão01
 		System.out.println("--------------------------------------");
 		
-		System.out.println("SEGUNDA QUESTAO");
-		int[] numeros = {1, 5, 3, 4, 2};
+		System.out.println("SEGUNDA QUESTAO"); //segunda questão
+		int[] numeros = {1, 5, 3, 4, 2}; //array com os valoeres 
 		
-		Questao02 questao02 = new Questao02();
-		questao02.encontraPares(numeros, 2);
+		Questao02 questao02 = new Questao02(); 
+		questao02.encontraPares(numeros, 2); // Metodo encontraPares da classe questão02
 
 		System.out.println("--------------------------------------");
-		System.out.println("TERCEIRA QUESTAO");
+		System.out.println("TERCEIRA QUESTAO"); //Terceira questão
 		Questao03 questao03 = new Questao03();
-		questao03.encriptar("Tenha um bom dia");
+		questao03.encriptar("Tenha um bom dia"); // frase para ser encriptada 
 	}
 
 }
